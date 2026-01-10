09e761c7efce25fff852a16993d44a9146a0119f
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-01-10 23:54:50 +0530

    convert to binaryy from bit manipulation

--- 09 Bit Manipulation/Introduction to Bit Manipulation.py
+++ 09 Bit Manipulation/Introduction to Bit Manipulation.py
@@ -1,1 +1,11 @@
 #Bit Manipulation
+"""1. Convert to Binary"""
+def convert2binary(num:int)->str:
+
+    result = ""
+    while num > 0:
+        if num % 2 == 1:
+            result+="1"
+        else:
+            result+="0"
+        num//2
