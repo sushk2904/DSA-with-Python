7e0cdce76a3bcafa082bdbe37bc0a2bef4382f4b
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-02-21 16:02:27 +0530

    feat: Initializing to study stacks and queues

---
+++ 11 Stacks and Queues/Implementing stack using array.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if len(self.items) == 0:
+            return "Cannot pop, stack is empty"
+        
+        x = self.items.pop()
+        return x
+    
+    def top(self):
+        if len(self.items) == 0:
+            return "Cannot pop, stack is empty"
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+    
+
+stack = Stack()
+
+stack.push(5)
+stack.push(10)
+stack.push(15)
+print(f"Stack content = {stack}")
+print(f"Stack popped item = {stack.pop()}")
+print(f"Current stack content = {stack}")
+print(f"Top item after pop {stack.top()}")
+print(f"stack is empty {stack.is_empty()}")
