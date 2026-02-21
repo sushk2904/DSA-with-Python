9176ff89681b266092853b69b4a8b3fd2ada87fd
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-02-21 17:02:03 +0530

    adding implementing queue using array

---
+++ 11 Stacks and Queues/02 Implementing queue using array.py
@@ -0,0 +1,42 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+    
+    def __repr__(self):
+        return self.items
+
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def enqueue(self, item):
+        self.items.append[item]
+
+    def dequeue(self):
+        if len(self.items) == 0:
+            return "Cannot pop, queue is empty"
+        x = self.items.pop(0)
+        return x
+    
+    def front(self):
+        if len(self.items) == 0:
+            return "Cannot peek, queue is empty"
+        
+        return self.items[0]
+    
+    def rear(self):
+        if len(self.items) == 0:
+            return "cannot read, queue is empty"
+        
+        return self.items[-1]
+    
+    def size(self):
+        if len(self.items) == 0:
+            return "cannot pop, queue is empty"
+        
+        return len(self.items)
+    
+    queue = Queue()
+    
+
+    
