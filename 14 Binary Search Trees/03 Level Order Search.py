ed1133de150d0d155505d60ed34f617c94bc53f6
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-06-03 23:48:52 +0530

    Removing the content beacause i want to write the code because of revision

--- 14 Binary Search Trees/03 Level Order Search.py
+++ 14 Binary Search Trees/03 Level Order Search.py
@@ -1,12 +1,2 @@
-from collections import deque
-def level_order(node):
-    result = []
-    queue = deque([])
-    queue.append(node)
-    while len(queue) != 0:
-        e = queue.popleft()
-        result.append(e.data)
-        if e.left is not None:
-            queue.append(e.left)
-        if e.right is not None:
+
 
