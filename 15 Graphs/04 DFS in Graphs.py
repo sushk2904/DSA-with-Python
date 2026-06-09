de8a7256ebac918b32a4574123e167cb4058bdd7
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-06-09 23:43:23 +0530

    DFS traversal learn through recursive techniques still the code isn't working fine but will decode it

--- 15 Graphs/04 DFS in Graphs.py
+++ 15 Graphs/04 DFS in Graphs.py
@@ -7,3 +7,5 @@ def dfs(node, result, visited, adj):
     
     print(adj)
 dfs(9, [], 1, [[],[2,8],[1,3,4],[2],[2,5],[4,6],[5,7],[6,8],[1,7,9], [8],],)
+
+"""The program isn't working i tried running even fixing it"""
