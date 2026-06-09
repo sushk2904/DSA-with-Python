588d2de6f39c7f53bf6cac012eef339f13a634c5
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-06-09 23:47:12 +0530

    [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] [[], [2, 8], [1, 3, 4], [2], [2, 5], [4, 6], [5, 7], [6, 8], [1, 7, 9], [8]] this is the output

--- 15 Graphs/04 DFS in Graphs.py
+++ 15 Graphs/04 DFS in Graphs.py
@@ -2,10 +2,10 @@ def dfs(node, result, visited, adj):
     visited[node] =  1
     result.append(node)
     for i in adj[node]:
-        if visited[i] == False:
-            dfs(i)
+        if visited[i] == 0:
+            dfs(i,result,visited,adj)
     
     print(adj)
-dfs(9, [], 1, [[],[2,8],[1,3,4],[2],[2,5],[4,6],[5,7],[6,8],[1,7,9], [8],],)
+dfs(9, [], [0,0,0,0,0,0,0,0,0,0], [[],[2,8],[1,3,4],[2],[2,5],[4,6],[5,7],[6,8],[1,7,9], [8],])
 
 """The program isn't working i tried running even fixing it"""
