19b443c0ef2156665f82e2ce0891896171f57dfc
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-07-27 23:50:14 +0530

    sorted the array recursion algo revised and corrected

--- 03 Recursion/07-sorting-algo-arr-list.py
+++ 03 Recursion/07-sorting-algo-arr-list.py
@@ -6,4 +6,16 @@ def checkSorted(l1):
         return ans
     else:
         return False
-print(checkSorted([3,55,7,11,39]))
+
+def checkSorted2(l2):
+    if len(l2) == 0 or len(l2) == 1:
+        return True
+    
+    if l2[0]<l2[1]:
+        return checkSorted2(l2[1:])
+    else:
+        return False
+
+l3 = [i for i in range(10000,1,-1)]
+print(checkSorted2(l3))
+print(checkSorted(l3))
