b256b22c518e9b88a314a5ad9572027d4dfb619e
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-08-01 23:56:43 +0530

    upper lower concept in binary search

--- 06 Binary Search/02 Concepts of Upper and Lower Bound.py
+++ 06 Binary Search/02 Concepts of Upper and Lower Bound.py
@@ -1,1 +1,15 @@
 #Concepts of Upper and Lower Bound
+target = 2
+nums =[1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n = len(nums)
+lb = -1
+low, high = 0, n-1
+while low<=high:
+    mid = (low+high)//2
+    if nums[mid]>=target:
+        lb = mid
+        high = mid-1
+    else:
+        low = mid+1
+
+print(lb)
