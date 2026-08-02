fbf91d8055f676b66196ea8c8d8eafedaf8fc82a
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-08-02 11:20:18 +0530

    finally corrected the upper bound code

--- 06 Binary Search/02 Concepts of Upper and Lower Bound.py
+++ 06 Binary Search/02 Concepts of Upper and Lower Bound.py
@@ -6,10 +6,25 @@ lb = -1
 low, high = 0, n-1
 while low<=high:
     mid = (low+high)//2
-    if nums[mid]>=target:
+    if nums[mid]>=target: #This is the most important thing to learn that >= target wali condition use hogi
         lb = mid
         high = mid-1
     else:
         low = mid+1
 
 print(lb)
+
+#Concept of Upper Bound
+target1 = 2
+nums1 =[1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n1 = len(nums1)
+ub = -1
+low1, high1 = 0, n1-1
+while low1 <= high1:
+    mid1 =  (low1+high1)//2
+    if nums1[mid1]>target1:
+        ub = mid1
+        high1 = mid1 -1
+    else:
+        low1 = mid1+1
+print(ub)
